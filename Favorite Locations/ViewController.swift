26466f55c4a105a8a7688743d5915c990ea1c286
Mikeias Nascimento <mikeias66@gmail.com>
2019-08-29 13:38:39 -0300

    Initial Commit

---
+++ Favorite Locations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Favorite Locations
+//
+//  Created by Mikeias Nascimento on 29/08/19.
+//  Copyright © 2019 Critical Techworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
