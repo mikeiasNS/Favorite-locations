b290afabfdca5028a8a21d53c97394e1e84f26b6
Mikeias Nascimento <mikeias66@gmail.com>
2019-08-29 14:48:59 -0300

    US-1 Basic list

--- Favorite Locations/LandingScreenVC.swift
+++ Favorite Locations/LandingScreenVC.swift
@@ -10,16 +10,38 @@ import UIKit
 import Alamofire
 
 class LandingScreenVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    var suggestions: [NSDictionary]? = nil
+    @IBOutlet var resultTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.suggestions?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = suggestions?[indexPath.row]["label"] as? String
+        cell.detailTextLabel?.text = suggestions?[indexPath.row]["countryCode"] as? String
+        
+        return cell
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //        TODO: get current Latitude, Longitude
+        let query = searchBar.text
+        let currentLatitude = -3.801865
+        let currentLongitude = -38.6215081
+        
+        Alamofire.request("http://autocomplete.geocoder.api.here.com/6.2/suggest.json?query=\(query ?? "")&app_id=GJExNBxZYOZ4V3w1KZQc&app_code=DD7aQwczIJTz0UwFt6HxOA&prox=\(currentLatitude),\(currentLongitude)&maxresults=20").responseJSON { (response) in
+            if let json = response.result.value as? [String: AnyObject] {
+                self.suggestions = json["suggestions"] as? [NSDictionary]
+                self.resultTableView.reloadData()
+            }
+        }
     }
 }
